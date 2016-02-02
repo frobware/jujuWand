16623661c678e97be8acea8ba5aa4f928ec1287a
James Tunnicliffe <james.tunnicliffe@nanosheep.org>
2016-02-02 13:42:06 +0000

    2to3 on testJuju.py

--- testJuju.py
+++ testJuju.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import os
 import subprocess
@@ -31,7 +31,7 @@ def main(args):
     rerun_filename = os.path.join(os.path.expanduser('~'),
                                   '.jujutestoutput_rerun.txt')
 
-    print 'Build duration:', datetime.now() - start_time
+    print('Build duration:', datetime.now() - start_time)
     start_time = datetime.now()
 
     filename = None
@@ -49,7 +49,7 @@ def main(args):
                 test_out, rc = run('GOMAXPROCS=32 go test github.com/juju/juju/' + package, write_to=f, fail_ok=True)
                 if rc:
                     exit(rc)
-        print 'Test duration:', datetime.now() - start_time
+        print('Test duration:', datetime.now() - start_time)
         start_time = datetime.now()
         exit(0)
 
@@ -60,7 +60,7 @@ def main(args):
         filename = output_filename
         with open(output_filename, 'w') as f:
             test_out, rc = run('go test ./...', write_to=f, fail_ok=True)
-            print 'Test duration:', datetime.now() - start_time
+            print('Test duration:', datetime.now() - start_time)
             start_time = datetime.now()
 
     else:
@@ -89,7 +89,7 @@ def main(args):
             unrecoverable = True
         elif line.startswith('# github.com'):
             unrecoverable = True
-            print line
+            print(line)
         else:
             s = re.search('FAIL\s+(github.com.*)\s[\d\.]+s\s*$', line)
             re_run.append(s.group(1))
@@ -97,19 +97,19 @@ def main(args):
     if len(re_run) == 0:
         return
 
-    print '-' * 20, 'Failing packages', '-' * 20
+    print('-' * 20, 'Failing packages', '-' * 20)
     for package in re_run:
-        print ' ', package
+        print(' ', package)
 
     if not unrecoverable:
-        print 'Re-running failed tests...'
+        print('Re-running failed tests...')
         with open(rerun_filename, 'w') as f:
             for package in re_run:
                 out, rc = run('go test ' + package, write_to=f, fail_ok=True)
-        print 'Re-run duration:', datetime.now() - start_time
+        print('Re-run duration:', datetime.now() - start_time)
         start_time = datetime.now()
     else:
-        print 'Some failures are unrecoverable... not re-running.'
+        print('Some failures are unrecoverable... not re-running.')
 
 
 if __name__ == '__main__':
